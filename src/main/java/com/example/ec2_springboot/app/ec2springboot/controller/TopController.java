08d4dd7283c282cbb1f3ffec5632f28375bce66d
jykym1006 <jykym1006@gmail.com>
2020-05-10 19:18:04 +0900

    top#index：appEnvを表示

--- src/main/java/com/example/ec2_springboot/app/ec2springboot/controller/TopController.java
+++ src/main/java/com/example/ec2_springboot/app/ec2springboot/controller/TopController.java
@@ -1,13 +1,19 @@
 package com.example.ec2_springboot.app.ec2springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TopController {
 
+  @Value("${app.env}")
+  private String appEnv;
+
   @RequestMapping("/")
-  public String index() {
+  public String index(Model model) {
+    model.addAttribute("appEnv", appEnv);
     return "index";
   }
 
